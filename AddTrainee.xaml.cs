d7d89dd4a9372770752ca1bb6651854cf1992c81
michaelatz <44846161+michaelatz@users.noreply.github.com>
2019-02-02 18:38:51 +0200

    Add files via upload

---
+++ AddTrainee.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PLWPF
+{
+    /// <summary>
+    /// Interaction logic for AddTrainee.xaml
+    /// </summary>
+    public partial class AddTrainee : Window
+    {
+        public AddTrainee()
+        {
+            InitializeComponent();
+        }
+         = new BE.Trainee();
+            this.gridAddDish.DataContext = dish;
+            bl = BL.FactoryBL.getBL();
+
+            dishSizeComboBox.ItemsSource = Enum.GetValues(typeof(BE.Size));
+            this.hechsherComboBox.ItemsSource = Enum.GetValues(typeof(BE.kosherLevel));
+
+private void freeDeliverNumTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
